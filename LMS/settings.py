7e2fd9ecf3f183e00f65110fb002df39d03b1cf8
Mountain-Michaelle <michaelchinemelu24@gmail.com>
2024-07-05 19:23:38 +0100

    added build folder

--- LMS/settings.py
+++ LMS/settings.py
@@ -165,10 +165,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:8000"
-]
 
 CORS_ALLOW_HEADERS = [
 'accept',

52e0eb9f039f39a85083fcb3ad22b8cb3c9b1c39
Mountain-Michaelle <michaelchinemelu24@gmail.com>
2024-07-05 18:42:53 +0100

    ready to go live

--- LMS/settings.py
+++ LMS/settings.py
@@ -82,12 +82,13 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#  'default': {
-#      'ENGINE': 'django.db.backends.sqlite3',
-#      'NAME': BASE_DIR / 'db.sqlite3',
-#  }
-#  }
+DATABASES = {
+ 'default': {
+     'ENGINE': 'django.db.backends.sqlite3',
+     'NAME': BASE_DIR / 'db.sqlite3',
+ }
+ }
+
    #   'default': {
 #     'ENGINE': 'django.db.backends.mysql',
 #     'NAME': 'mysql',
@@ -96,19 +97,6 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 #     'HOST': '127.0.0.1',
 #     'PORT': '3306',
 #     }
-      
-    
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lms_testing1',
-        'USER': 'postgres',
-        'PASSWORD': '41240075',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -164,6 +152,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
